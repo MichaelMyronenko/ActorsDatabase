ab0b5a86906a57987a65b2b10da559ff7ee9a14d
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-06 15:59:41 +0300

    #4 Fix - fixed select_actors_with_unique_lastname.sql query

--- select_actors_with_unique_lastname.sql
+++ select_actors_with_unique_lastname.sql
@@ -1,8 +1,34 @@
 # выбрать список актёров, не имеющие однофамильцев. Вывести список в виде:
 # “Полное имя актёра” (одной колонкой);
 
-SELECT CONCAT(A.firstname, ' ', A.lastname) AS actor_fullname
-FROM actors A
-         JOIN actors B on A.birthdate = B.birthdate
-GROUP BY actor_fullname
-HAVING COUNT(B.lastname) = 1;
+#Solution 1
+
+CREATE TEMPORARY TABLE IF NOT EXISTS temporary_actors_lastnames
+SELECT lastname
+FROM actors
+GROUP BY lastname
+HAVING COUNT(lastname) = 1;
+
+SELECT CONCAT(actors.firstname, ' ', actors.lastname) AS fullname
+FROM actors
+         INNER JOIN temporary_actors_lastnames tal on actors.lastname = tal.lastname
+GROUP BY fullname;
+
+#Solution 2
+
+SELECT CONCAT(actors.firstname, ' ', actors.lastname) AS fullname
+FROM actors
+         INNER JOIN
+     (SELECT lastname
+      FROM actors
+      GROUP BY lastname
+      HAVING COUNT(lastname) = 1) AS lastnames
+     on actors.lastname = lastnames.lastname
+GROUP BY fullname;
+
+# Solution 3
+
+SELECT CONCAT(GROUP_CONCAT(actors.firstname), ' ', actors.lastname) AS fullname
+FROM actors
+GROUP BY actors.lastname
+HAVING COUNT(actors.lastname) = 1;
