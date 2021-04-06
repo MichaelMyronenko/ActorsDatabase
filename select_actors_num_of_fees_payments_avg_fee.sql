28b1e6adb5b39ad539993ae32a82d32df20bc731
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-06 12:22:31 +0300

    #3 Fix - fixed select_actors_num_of_fees_payments_avg_fee.sql query

--- select_actors_num_of_fees_payments_avg_fee.sql
+++ select_actors_num_of_fees_payments_avg_fee.sql
@@ -3,16 +3,16 @@
 # Вывести список в порядке уменьшения среднего гонорара студии актёрам. Результат оформить в виде таблицы со следующими колонками:
 # “Название студии”, “Кол-во фильмов”, “Кол-во выплат”, “Сумма гонораров”, “Средний гонорар”.
 
-SELECT s.name                           AS studio_name,
-       CONCAT(firstname, ' ', lastname) AS fullname,
-       COUNT(DISTINCT m.id)             AS number_of_films,
-       SUM(af.fee)                      AS sum_of_fees,
-       COUNT(DISTINCT af.id)            AS number_of_payments,
-       AVG(af.fee)                      AS average_fee
+SELECT s.name                AS studio_name,
+#        CONCAT(firstname, ' ', lastname) AS fullname,
+       COUNT(DISTINCT m.id)  AS number_of_films,
+       SUM(af.fee)           AS sum_of_fees,
+       COUNT(DISTINCT af.id) AS number_of_payments,
+       AVG(af.fee)           AS average_fee
 FROM actors
          INNER JOIN actors_fees af on actors.id = af.actor_id
          INNER JOIN movies m on af.movie_id = m.id
+    AND TIMESTAMPDIFF(year, m.release_date, now()) <= 10
          INNER JOIN studios s on m.studio_id = s.id
-WHERE TIMESTAMPDIFF(year, m.release_date, now()) < 10
-GROUP BY s.name, fullname
+GROUP BY s.name, actors.id
 ORDER BY average_fee DESC;
